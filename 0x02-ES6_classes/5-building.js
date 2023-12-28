874085d24e457328dda83208e507a29ffec1740b
Michael Appiah Dankwah <michaelappiah2018@icloud.com>
2023-12-28 20:47:13 +0000

    Update

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,16 +1,12 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor !== Building && typeof this.evacuationWarningMesagge !== 'function') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+    if (new.target !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(sqft) {
-    this._sqft = sqft;
   }
 }
